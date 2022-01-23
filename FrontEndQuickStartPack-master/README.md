ca8ae1887ef41a55910be6f515c72371b3aba00d
sonaly-mehr <sonaly.mehr@gmail.com>
2022-01-23 14:46:56 +0600

    first commit

---
+++ FrontEndQuickStartPack-master/README.md
@@ -0,0 +1,35 @@
+# Front End Development Quick Start pack v2.0.2
+---
+
+
+### Features
+--- 
+
+* Boilerplate html5
+* Bootstrap v4.5.0
+* FontAwesome v5.9.0
+* Normalize Css
+* JQuery v3.5.1
+* 404 page
+* Favicon Icon
+
+### Extra Plugin
+---
+
+* WoW.js
+* Animate.Css
+* Hover Css
+* Owl Carousel v2.3.4
+* Magnific Popup
+
+
+
+### Domain and Hosting Services Recommendation
+---
+[UnivaHost](https://univahost.com) is our top-rated web hosting company because their last 99% uptime and speed is very strong! so we recommend you can use [UnivaHost](https://univahost.com) Services 
+
+
+
+### License
+---
+The Quick Start Pack is open-source licensed under the  [MIT license](https://opensource.org/licenses/MIT).
---
+++ js/main.js
@@ -0,0 +1,303 @@
+$(document).ready(function(){
+/*=====Owl-carousel======*/
+$('.brand-item').owlCarousel({
+        
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+});
+
+  /*======Filter=====*/
+  $(document).ready(function(){
+     var container = document.querySelector('.gallary');
+       
+      var mixer = mixitup(container, {
+          selectors: {
+              control: '[our-mix-control]'
+          }
+      });
+
+});
+      /*=====Magnefic-PopUp =====*/
+$(document).ready(function(){
+      $('#video-link').magnificPopup({
+        type:'iframe',
+
+        iframe: {
+
+  patterns: {
+    youtube: {
+      index: 'youtube.com/', 
+
+      id: 'v=',
+
+      src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+    },
+
+  srcAction: 'iframe_src', 
+}
+    }
+
+});
+});
+
+$(document).ready(function(){ 
+    /*===Counter ===*/
+     $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+});
+
+$(document).ready(function(){
+
+  //Client-Review
+
+$('.client-review').owlCarousel({
+        
+        loop:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+ });
+
+$(document).ready(function(){
+
+  //Client-Review
+
+$('.team-wrap').owlCarousel({
+        
+        loop:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
+ });
+
+
+/*=====Skill-Progress-Bar=====*/
+
+$(document).ready(function(){
+   
+   (function ($) {
+
+    $.fn.barfiller = function (options) {
+
+        var defaults = $.extend({
+            barColor: '#16b597',
+            tooltip: true,
+            duration: 1000,
+            animateOnResize: true,
+            symbol: "%"
+        }, options);
+
+
+        /******************************
+        Private Variables
+        *******************************/         
+
+        var object = $(this);
+        var settings = $.extend(defaults, options);
+        var barWidth = object.width();
+        var fill = object.find('.fill');
+        var toolTip = object.find('.tip');
+        var fillPercentage = fill.attr('data-percentage');
+        var resizeTimeout;
+        var transitionSupport = false;
+        var transitionPrefix;
+
+        /******************************
+        Public Methods
+        *******************************/         
+        
+        var methods = {
+
+            init: function() {
+                return this.each(function () {
+                    if(methods.getTransitionSupport()) {
+                        transitionSupport = true;
+                        transitionPrefix = methods.getTransitionPrefix();
+                    }
+
+                    methods.appendHTML();
+                    methods.setEventHandlers();
+                    methods.initializeItems();
+                });
+            },
+
+            /******************************
+            Append HTML
+            *******************************/      
+
+            appendHTML: function() {
+                fill.css('background', settings.barColor);
+
+                if(!settings.tooltip) {
+                    toolTip.css('display', 'none');
+                }
+                toolTip.text(fillPercentage + settings.symbol);
+            },
+            
+
+            /******************************
+            Set Event Handlers
+            *******************************/
+            setEventHandlers: function() {
+                if(settings.animateOnResize) {
+                    $(window).on("resize", function(event){
+                        clearTimeout(resizeTimeout);
+                        resizeTimeout = setTimeout(function() { 
+                        methods.refill(); 
+                        }, 300);
+                    });       
+                }
+            },        
+
+            /******************************
+            Initialize
+            *******************************/      
+
+            initializeItems: function() {
+            var pctWidth = methods.calculateFill(fillPercentage);
+            object.find('.tipWrap').css({ display: 'inline' });
+
+            if(transitionSupport)
+                methods.transitionFill(pctWidth);
+            else
+                methods.animateFill(pctWidth);
+            },
+
+            getTransitionSupport: function() {
+
+                var thisBody = document.body || document.documentElement,
+                thisStyle = thisBody.style;
+                var support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
+                return support;   
+            },
+                
+            getTransitionPrefix: function() {
+                if(/mozilla/.test(navigator.userAgent.toLowerCase()) && !/webkit/.test(navigator.userAgent.toLowerCase())) {
+                    return '-moz-transition';
+                }
+                if(/webkit/.test(navigator.userAgent.toLowerCase())) {
+                    return '-webkit-transition';
+                }
+                if(/opera/.test(navigator.userAgent.toLowerCase())) {
+                    return '-o-transition';
+                }
+                if (/msie/.test(navigator.userAgent.toLowerCase())) {
+                    return '-ms-transition';
+                }
+                else {
+                    return 'transition';
+                }
+            },
+
+            getTransition: function(val, time, type) {
+
+                var CSSObj;
+                if(type === 'width') {
+                    CSSObj = { width : val };
+                }
+                else if (type === 'left') {
+                    CSSObj = { left: val };
+                }
+
+                time = time/1000;
+                CSSObj[transitionPrefix] = type+' '+time+'s ease-in-out';       
+                return CSSObj;
+
+            },        
+
+            refill: function() {
+                fill.css('width', 0);
+                toolTip.css('left', 0);
+                barWidth = object.width();
+                methods.initializeItems();
+            },
+
+            calculateFill: function(percentage) {
+                percentage = percentage *  0.01;
+                var finalWidth = barWidth * percentage;
+                return finalWidth;
+            },       
+
+            transitionFill: function(barWidth) {
+
+                var toolTipOffset = barWidth - toolTip.width();
+                fill.css( methods.getTransition(barWidth, settings.duration, 'width'));
+                toolTip.css( methods.getTransition(toolTipOffset, settings.duration, 'left'));
+
+            },  
+
+            animateFill: function(barWidth) {
+                var toolTipOffset = barWidth - toolTip.width();
+                fill.stop().animate({width: '+=' + barWidth}, settings.duration);
+                toolTip.stop().animate({left: '+=' + toolTipOffset}, settings.duration);
+            }
+      
+        };
+        
+        if (methods[options]) {   // $("#element").pluginName('methodName', 'arg1', 'arg2');
+            return methods[options].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof options === 'object' || !options) {   // $("#element").pluginName({ option: 1, option:2 });
+            return methods.init.apply(this);  
+        } else {
+            $.error( 'Method "' +  method + '" does not exist in barfiller plugin!');
+        } 
+    };
+
+})(jQuery);
+
+});
+
+
+/*======Stickey-Nav=======*/
+
+$(document).ready(function(){
+ $(window).scroll(function(){
+     
+        var ourWindow = $(this).scrollTop();
+        
+        if(ourWindow>300) {
+            $('body').addClass('sticky');
+        } else{
+            $('body').removeClass('sticky');
+        }
+    });
+
+});
+
+
+
+
